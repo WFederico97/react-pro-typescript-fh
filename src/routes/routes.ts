360a3f632c2b9388e6ac5cc977d0172474c38c16
Federico W <wuthrich09@gmail.com>
2024-01-03 22:23:00 -0300

    SECC-4 LECC-10

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,6 @@
 //* Utilizamos como directorio de rutas y usarlo dinamicamente , todo lo que agreguemos aca , se agrega a las routes de navigation 
 import { LazyExoticComponent, lazy } from "react";
+import { NoLazy } from "../01-Lazyload/pages/NoLazy";
 // import { LazyLoad1,LazyLoad2,LazyLoad3 } from "../01-Lazyload/pages";
 
 type JSXComponent = () => JSX.Element
@@ -13,28 +14,21 @@ interface Route {
     
 }
 
-const Lazy1 = lazy(()=> import('../01-Lazyload/pages/LazyLoad1'))
-const Lazy2 = lazy(()=> import('../01-Lazyload/pages/LazyLoad2'))
-const Lazy3 = lazy(()=> import('../01-Lazyload/pages/LazyLoad3'))
+const LazyLayout = lazy(()=> import('../01-Lazyload/layout/LazyLayout'))
+
 
 export const routes: Route[] = [
     {
-        to: '/lazy1',
-        path: 'lazy1',
-        Component: Lazy1,
-        name: 'Lazy 1'
+        to: '/lazyload/',
+        path: '/lazyload/*',
+        Component: LazyLayout,
+        name: 'Lazy Layout'
     },
     {
-        to: '/lazy2',
-        path: 'lazy2',
-        Component: Lazy2,
-        name: 'Lazy 2'
-    },
-    {
-        to: '/lazy3',
-        path: 'lazy3',
-        Component: Lazy3,
-        name: 'Lazy 3'
-    },
+        to: '/no-lazy',
+        path: 'no-lazy',
+        Component: NoLazy,
+        name: 'No Lazy'
+    }
 
 ];
